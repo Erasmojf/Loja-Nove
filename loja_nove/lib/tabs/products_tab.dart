5887ae4c7a2e03ea1719d25b16a89e144446764b
Erasmo J.F Da Silva <65982001+wingsentregas@users.noreply.github.com>
2021-03-18 09:06:14 -0300

    CM - Criação  de tela das categorias e  class produto
    Criação  de tela das categorias e  class produto

--- loja_nove/lib/tabs/products_tab.dart
+++ loja_nove/lib/tabs/products_tab.dart
@@ -1,26 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_nove/tiles/category_tile.dart';
 
 class ProductsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<QuerySnapshot>(
-      future: FirebaseFirestore.instance.collection("products").get(),
-        builder: (context, snapshot){
-          if(snapshot.hasData){
+        future: FirebaseFirestore.instance.collection("products").get(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
             return Center(
-              child: CircularProgressIndicator(
-
-              ),
+              child: CircularProgressIndicator(),
             );
-          }else{
-            return ListView(
-              children: [
+          } else {
+            var dividerTile = ListTile.divideTiles(
+                    tiles: snapshot.data.docs.map((doc) {
+                      return CategoryTile(doc);
+                    }).toList(),
+                    color: Colors.grey[500])
+                .toList();
 
-              ],
+            return ListView(
+              children: dividerTile,
             );
           }
-        }
-    );
+        });
   }
 }

1780e8cd0219cd40e67f41a4d9dc21f25a471fc4
Erasmo J.F Da Silva <65982001+wingsentregas@users.noreply.github.com>
2021-03-19 10:57:05 -0300

    CM - adicionado novas funcionalidades no card produto
    Adicionado novas funcionalidades no card produto

--- loja_nove/lib/tiles/product_tile.dart
+++ loja_nove/lib/tiles/product_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_nove/datas/product_data.dart';
+import 'package:loja_nove/screens/product_screen.dart';
 
 class ProductTile extends StatelessWidget {
   final String type;
@@ -8,6 +9,11 @@ class ProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: (){
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context)=> ProductScreen(product)),
+        );
+      },
       child: Card(
         child: type == "grid"
             ? Column(
@@ -39,14 +45,50 @@ class ProductTile extends StatelessWidget {
                               fontSize: 14.0,
                               fontWeight: FontWeight.bold,
                             ),
-                          )
+                          ),
                         ],
                       ),
                     ),
                   ),
                 ],
               )
-            : Row(),
+            : Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                      child: Image.network(
+                        product.images[0],
+                        fit: BoxFit.cover,
+                        height: 250.0,
+                      ),
+                  ),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                           crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                product.title,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                "R\$ ${product.price.toStringAsFixed(2)}",
+                                style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                        ),
+                      ),
+                  ),
+                ],
+              ),
       ),
     );
   }

58c7dab756e6a364d4c503c408a6f57415b8159c
Erasmo J.F Da Silva <65982001+wingsentregas@users.noreply.github.com>
2021-03-22 20:11:08 -0300

    CM - Criado o Cart Screen

--- loja_nove/lib/screens/cart_screen.dart
+++ loja_nove/lib/screens/cart_screen.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:loja_nove/models/cart_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
+class CartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meu carinho'),
+        actions: [
+          Container(
+            padding: EdgeInsets.only(right: 10.0),
+            child: ScopedModelDescendant<CartModel>(
+                builder: (context, child, model) {
+              int p = model.product.length;
+              return Text('${p ?? 0} ${p == 1 ? 'ITEM' : 'ITENS'}');
+            }),
+          )
+        ],
+      ),
+    );
+  }
+}
